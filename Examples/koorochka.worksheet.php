2ae33f447bf893ffde142f739142c03efef69a20
artemkoorochka <artem@koorochka.com>
2020-02-04 13:40:41 +0200

    Кеширование на диске

--- Examples/koorochka.worksheet.php
+++ Examples/koorochka.worksheet.php
@@ -12,24 +12,14 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.ph
 require_once dirname(__FILE__) . '/../Classes/PHPExcel.php';
 
 
-
-$fileName = $_SERVER["DOCUMENT_ROOT"] . "/upload/excel/40b/40b446e24e0fa244d1ec3132a7b763b6.xls";
-$inputFileName = $fileName;
-$fileContent = "";
-
-//get inputFileType (most of time Excel5)
-$inputFileType = PHPExcel_IOFactory::identify($inputFileName);
-
-//initialize cache, so the phpExcel will not throw memory overflow
-$cacheMethod = PHPExcel_CachedObjectStorageFactory::cache_to_phpTemp;
-$cacheSettings = array(' memoryCacheSize ' => '8MB');
+$cacheMethod = PHPExcel_CachedObjectStorageFactory::cache_to_discISAM;
+$cacheSettings = array(
+    'dir' => $_SERVER["DOCUMENT_ROOT"] . "/upload/excel/cache"
+);
 PHPExcel_Settings::setCacheStorageMethod($cacheMethod, $cacheSettings);
 
-//initialize object reader by file type
-$objReader = PHPExcel_IOFactory::createReader($inputFileType);
+// Create new PHPExcel object
+$objReader = PHPExcel_IOFactory::createReader('Excel5');
+$objPHPExcel = $objReader->load($_SERVER["DOCUMENT_ROOT"] . "/upload/excel/40b/40b446e24e0fa244d1ec3132a7b763b6.xls");
 
-//read only data (without formating) for memory and time performance
-$objReader->setReadDataOnly(true);
 
-//load file into PHPExcel object
-$objPHPExcel = $objReader->load($inputFileName);
