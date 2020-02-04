f417beb0e41217bc66f438b7333023ab3a0cdf64
artemkoorochka <artem@koorochka.com>
2020-02-04 17:44:19 +0200

    Use chunks

--- Examples/koorochka.worksheet.php
+++ Examples/koorochka.worksheet.php
@@ -1,26 +1,41 @@
 <?
 /**
  * https://github.com/PHPOffice/PHPExcel/blob/develop/Documentation/markdown/Overview/04-Configuration-Settings.md
+ * system/7studio/excel/git/PHPExcel/Examples/24readfilter.php
  */
 if (ini_get('mbstring.func_overload') & 2) {
     ini_set("mbstring.func_overload", 0);
 }
 
+require_once dirname(__FILE__) . '/../Classes/PHPExcel/IOFactory.php';
+
+class MyReadFilter implements PHPExcel_Reader_IReadFilter
+{
+    public function readCell($column, $row, $worksheetName = '') {
+        // Read title row and rows 20 - 30
+        if ($row == 1 || ($row >= 20 && $row <= 30)) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
 $arParams = array();
 $arResult = array();
 
-//require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 
 /** Include PHPExcel */
-require_once dirname(__FILE__) . '/../Classes/PHPExcel.php';
 
 
 // Create new PHPExcel object
 $objReader = PHPExcel_IOFactory::createReader('Excel5');
-$objPHPExcel = $objReader->load($_SERVER["DOCUMENT_ROOT"] . "/upload/excel/40b/40b446e24e0fa244d1ec3132a7b763b6.xls");
+$objReader->setReadFilter( new MyReadFilter() );
+$objPHPExcel = $objReader->load($_SERVER["DOCUMENT_ROOT"] . "/upload/excel/sample/test1.xls");
 
-$cacheMethod = PHPExcel_CachedObjectStorageFactory::cache_to_discISAM;
-$cacheSettings = array(
-    'dir' => $_SERVER["DOCUMENT_ROOT"] . "/upload/excel/cache"
-);
-PHPExcel_Settings::setCacheStorageMethod($cacheMethod, $cacheSettings);
+
+/////////
+///
+
+
