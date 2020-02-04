92c36f5bc36671a0d622e442b86fdbf6b766c92c
artemkoorochka <artem@koorochka.com>
2020-02-04 13:43:55 +0200

    Cache

--- Examples/koorochka.worksheet.php
+++ Examples/koorochka.worksheet.php
@@ -1,4 +1,7 @@
 <?
+/**
+ * https://github.com/PHPOffice/PHPExcel/blob/develop/Documentation/markdown/Overview/04-Configuration-Settings.md
+ */
 if (ini_get('mbstring.func_overload') & 2) {
     ini_set("mbstring.func_overload", 0);
 }
@@ -6,20 +9,18 @@ if (ini_get('mbstring.func_overload') & 2) {
 $arParams = array();
 $arResult = array();
 
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+//require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 
 /** Include PHPExcel */
 require_once dirname(__FILE__) . '/../Classes/PHPExcel.php';
 
+
+// Create new PHPExcel object
+$objReader = PHPExcel_IOFactory::createReader('Excel5');
+$objPHPExcel = $objReader->load($_SERVER["DOCUMENT_ROOT"] . "/upload/excel/40b/40b446e24e0fa244d1ec3132a7b763b6.xls");
 
 $cacheMethod = PHPExcel_CachedObjectStorageFactory::cache_to_discISAM;
 $cacheSettings = array(
     'dir' => $_SERVER["DOCUMENT_ROOT"] . "/upload/excel/cache"
 );
 PHPExcel_Settings::setCacheStorageMethod($cacheMethod, $cacheSettings);
-
-// Create new PHPExcel object
-$objReader = PHPExcel_IOFactory::createReader('Excel5');
-$objPHPExcel = $objReader->load($_SERVER["DOCUMENT_ROOT"] . "/upload/excel/40b/40b446e24e0fa244d1ec3132a7b763b6.xls");
-
-
